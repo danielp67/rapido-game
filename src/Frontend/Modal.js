fbb7ed8a78b3af298990cd4d23f982c51a6fe647
danielp67 <66970422+danielp67@users.noreply.github.com>
2022-11-29 20:50:15 +0100

    refactor with modal component

--- src/Frontend/Modal.js
+++ src/Frontend/Modal.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import {Theme, ThemeContext} from "./ThemeContext";
 
-const Modal = (props) => {
-
-    const {title, content} = props
+const Modal = ({id, children}) => {
 
     return (
 
             <ThemeContext.Consumer>
                 {({theme}) => (
-            <div className="modal fade" id="exampleModalToggle2" aria-hidden="true"
-                 aria-labelledby="exampleModalToggleLabel2" tabIndex="-1">
+            <div className="modal fade" id={id} aria-hidden="true"
+                 aria-labelledby={id} tabIndex="-1">
                 <div className="modal-dialog modal-dialog-centered">
                     <div className={`modal-content ${theme.className}`}>
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalToggleLabel2">
-                                <i className="fa fa-cog" aria-hidden="true"/> {title}
-                            </h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"/>
-                        </div>
-                        <div className="modal-body">
-                            {content}
-
-                        </div>
+                        {children}
                     </div>
                 </div>
             </div>
--- src/Frontend/SecondMenu.js
+++ src/Frontend/SecondMenu.js
@@ -1,51 +1,48 @@
 import React from 'react';
 import SettingsMenu from "./SettingsMenu";
+import Modal from "./Modal";
 
 const SecondMenu = (props) => {
 
     const {startGame} = props
 
-        return (
+    return (
 
-            <>
+        <>
+            <button type="button" className="btn btn-primary col-4 m-2" data-bs-toggle="modal"
+                    data-bs-target="#staticBackdropSecond">
+                <i className="fa fa-play" aria-hidden="true"/>
+            </button>
 
-                <button type="button" className="btn btn-primary col-4 m-2" data-bs-toggle="modal" data-bs-target="#staticBackdropSecond">
-                    <i className="fa fa-play" aria-hidden="true"/>
-                </button>
+            <Modal id={"staticBackdropSecond"}>
 
-                <div className="modal fade" id="staticBackdropSecond" aria-hidden="true"
-                     aria-labelledby="exampleModalToggleLabel" tabIndex="-1">
-                    <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                                    <i className="fa fa-play" aria-hidden="true"/> Menu
-                                </h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal"
-    aria-label="Close"/>
-                            </div>
-                            <div className="modal-body">
-                                <h4>Rapido game</h4>
-                            </div>
-                            <div className="modal-footer">
-                                <button className="btn btn-primary" data-bs-target="#exampleModalToggle2"
-                                        data-bs-toggle="modal">
-                                    <i className="fa fa-cog" aria-hidden="true"/> Settings
-                                </button>
-
-                                <button className="btn btn-primary" data-bs-dismiss="modal" onClick={() => startGame(true)}>
-                                    <i className="fa fa-play" aria-hidden="true"/> Start
-                                </button>
-
-                            </div>
-                        </div>
-                    </div>
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                        <i className="fa fa-play" aria-hidden="true"/> Menu
+                    </h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"/>
                 </div>
+                <div className="modal-body">
+                    <h4>Rapido game</h4>
+                </div>
+                <div className="modal-footer">
+                    <button className="btn btn-primary" data-bs-target="#exampleModalToggle2"
+                            data-bs-toggle="modal">
+                        <i className="fa fa-cog" aria-hidden="true"/> Settings
+                    </button>
+
+                    <button className="btn btn-primary" data-bs-dismiss="modal" onClick={() => startGame(true)}>
+                        <i className="fa fa-play" aria-hidden="true"/> Start
+                    </button>
+
+                </div>
+
+            </Modal>
 
                 <SettingsMenu/>
 
             </>
-        )
-}
+            )
+            }
 
-export default SecondMenu;
+            export default SecondMenu;
