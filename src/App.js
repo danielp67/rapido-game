a5391d97126b99f5e61b5876e0a4bbbd001d6af6
danielp67 <66970422+danielp67@users.noreply.github.com>
2022-12-02 22:14:01 +0100

    feature on launch menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MainBoard from "./Frontend/MainBoard";
 import {ThemeContext, Theme} from "./Frontend/ThemeContext";
 import React, {useReducer, useState} from "react";
+import Modal from "./Frontend/Modal";
 
 
 class App extends React.Component {
@@ -20,15 +21,60 @@ class App extends React.Component {
         this.state = {
             theme: Theme.light,
             toggleTheme: this.toggleTheme,
+            name:"launch"
         };
+    }
+
+
+    setClassName = (props) => {
+        if(props)
+        {
+            this.setState({...this.state, name:"" })
+        }
+
     }
 
     render() {
 
         return (
             <ThemeContext.Provider value={this.state}>
-                <div className={`container-fluid vh-100 ${this.state.theme.className}`}>
+                <div className={`container-fluid vh-100 ${this.state.theme.className} ${this.state.name}`}>
 
+
+                    <div className={"overlay"}>
+                        <div className="card launch-card text-center my-auto">
+                            <div className="card-header">
+                                <ul className="nav nav-tabs card-header-tabs">
+                                <li className="nav-item">
+                                        <a className="nav-link active" href="#">Active</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled">Score</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Rapido game</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional
+                                    content.</p>
+                            </div>
+                            <div className="card-footer">
+                                <button className="btn btn-primary" data-bs-target="#exampleModalToggle2"
+                                        data-bs-toggle="modal">
+                                    <i className="fa fa-cog" aria-hidden="true"/> Settings
+                                </button>
+
+                                <button className="btn btn-primary" onClick={() => this.setClassName(true)}>
+                                    <i className="fa fa-play" aria-hidden="true"/> Start
+                                </button>
+
+                            </div>
+
+                        </div>
+                    </div>
                     <MainBoard/>
                 </div>
 
