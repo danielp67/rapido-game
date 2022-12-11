69d4e85f82f17b0fc2ad05c39a4c0c78bda7842a
danielp67 <66970422+danielp67@users.noreply.github.com>
2022-12-11 13:38:11 +0100

    correction

--- src/Components/SortedSlot.js
+++ src/Components/SortedSlot.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 const SortedSlot = (props) => {
 
     const {slotIndex, setSelectedSlot, droppedCard} = props
-    const[enter, setEnter]=  useState(false)
+    const [enter, setEnter] = useState(false)
 
     const dragEnter = () => {
         setSelectedSlot(slotIndex)
@@ -14,34 +14,31 @@ const SortedSlot = (props) => {
         setEnter(false)
     };
 
-if(enter)
-{
+    if (enter) {
         return (
             <div className={`col-2 col-md-2 bg-secondary bg-opacity-50`}
                  onDragEnter={() => dragEnter()}
                  onDragLeave={() => dragLeave()}
             >
-                    <div className={`card sortedSlotCard border border-dark border-2 my-1 bg-${droppedCard[0].suit}`}>
-                        <p className="my-auto fs-2 fw-bold">{droppedCard[0].value}</p>
+                <div className={`card sortedSlotCard border border-dark border-2 my-1 bg-${droppedCard[0].suit}`}>
+                    <p className="my-auto fs-2 fw-bold">{droppedCard[0].value}</p>
 
-                    </div>
                 </div>
+            </div>
         )
 
-    }
-else
-{
-    return (
-        <div className={`col-2 col-md-2`}
-             onDragEnter={() => dragEnter()}
-        >
-            <div className={`card sortedSlotCard my-1 bg-${droppedCard[0].suit}`}>
-                <p className="my-auto fs-2 fw-bold">{droppedCard[0].value}</p>
+    } else {
+        return (
+            <div className={`col-2 col-md-2`}
+                 onDragEnter={() => dragEnter()}
+            >
+                <div className={`card sortedSlotCard my-1 bg-${droppedCard[0].suit}`}>
+                    <p className="my-auto fs-2 fw-bold">{droppedCard[0].value}</p>
 
-        </div>
-        </div>
-    )
-}
+                </div>
+            </div>
+        )
+    }
 
 }
 
